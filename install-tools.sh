db12cea86c359b2b66eddd9c5cbbb93194d2123a
Shu-Hao Liu <aidan.park888@proton.me>
2025-01-05 17:28:29 -0600

    Create install-tools.sh

---
+++ install-tools.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt update
+sudo apt install -y build-essential libssl-dev libpcap-dev pkg-config \
+  libnl-3-dev libnl-genl-3-dev checkinstall git
+sudo apt install -y aircrack-ng
+sudo apt install -y crunch
+git clone https://github.com/Mebus/cupp.git
+cd cupp
+chmod +x cupp.py
+echo "Completed installing tools."
