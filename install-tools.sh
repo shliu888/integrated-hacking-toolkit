6e516a3a598daa4ae7989eca5408be5a1efab167
Shu-Hao Liu <aidan.park888@proton.me>
2025-01-05 17:31:14 -0600

    Update install-tools.sh

--- install-tools.sh
+++ install-tools.sh
@@ -5,6 +5,7 @@ sudo apt install -y build-essential libssl-dev libpcap-dev pkg-config \
   libnl-3-dev libnl-genl-3-dev checkinstall git
 sudo apt install -y aircrack-ng
 sudo apt install -y crunch
+sudo apt install -y dsniff
 git clone https://github.com/Mebus/cupp.git
 cd cupp
 chmod +x cupp.py
