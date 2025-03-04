f3d67d7af88ac24e295ec0f50c4084e07a23bd21
Shu-Hao Liu <aidan.park888@proton.me>
2025-03-04 17:59:41 -0600

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
+
+git clone https://github.com/Mebus/cupp.git
+cd cupp
+chmod +x cupp.py
+sudo apt update
+sudo apt install theharvester -y
+sudo apt install recon-ng -y
+sudo apt install whois -y
+sudo apt install -y build-essential libssl-dev libpcap-dev pkg-config \
+libnl-3-dev libnl-genl-3-dev checkinstall git
+sudo apt install java-wrapper
 chmod +x hackingtoolkit
 sudo mv hackingtoolkit /usr/local/bin
+echo "Installation complete."
